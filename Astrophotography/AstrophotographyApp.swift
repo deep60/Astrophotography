e7d85d6344751434ff2536f8cde0efad75fd96bf
deep60 <deep6060p@gmail.com>
2024-12-21 05:05:17 +0530

    Initial Commit

---
+++ Astrophotography/AstrophotographyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstrophotographyApp.swift
+//  Astrophotography
+//
+//  Created by P Deepanshu on 21/12/24.
+//
+
+import SwiftUI
+
+@main
+struct AstrophotographyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
